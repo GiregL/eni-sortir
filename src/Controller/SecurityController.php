566671cbeca0e7c9176b33e2fc3aea8a0ba1ad52
julienaverbouch <julienaverbouch@gmail.com>
2023-02-21 10:14:27 +0100

    ajout des messages d'erreur + menu connexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -20,6 +20,7 @@ class SecurityController extends AbstractController
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+        $error == true ? $error = $this->addFlash("error", "l'email ou le mot de passe sont incorrects") : $error = $this->addFlash("success", "Vous êtes connecté");
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
