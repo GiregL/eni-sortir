bcd5bc0cab97856ffbc57828b12ed22790b6973a
julienaverbouch <julienaverbouch@gmail.com>
2023-02-24 15:17:07 +0100

    correctif

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -49,6 +49,9 @@ class MainController extends AbstractController
         $twig->addFunction(new TwigFunction("isUserOrganizerOfEvent", function ($user, $event) {
             return $this->eventServices->isUserOrganizerOfEvent($user, $event);
         }));
+        $twig->addFunction(new TwigFunction("isPublish", function ($event) {
+            return $this->eventServices->isPublished($event);
+        }));
 
         return new Response($twig->render('main/index.html.twig', [
             "eventFilterForm" => $form->createView(),
