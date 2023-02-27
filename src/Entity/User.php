e981c7fbd8ce9c861c31a0ebd7793320bf31102c
Lezenn <gireg.lezoraine2022@campus-eni.fr>
2023-02-27 12:27:43 +0100

    Is admin user.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -153,4 +153,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    /**
+     * @return bool True if the user is an admin
+     */
+    public function isAdmin(): bool
+    {
+        return in_array("ROLE_ADMIN", $this->getRoles());
+    }
 }
